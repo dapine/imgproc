dc76ddd087e9fd24d70f5623b20f9c795fd44ea8
Breno Pinheiro <davepinh@gmail.com>
2022-05-19 10:12:59 -0300

    Add `Convert` function and tests

--- image/image.go
+++ image/image.go
@@ -7,6 +7,49 @@ import (
 	"github.com/h2non/bimg"
 )
 
+type ImageType bimg.ImageType
+
+var stringToImageType = map[string]ImageType {
+	"jpeg": 1,
+	"jpg": 1,
+	"webp": 2,
+	"png": 3,
+	"tiff": 4,
+	"gif": 5,
+	"pdf": 6,
+	"svg": 7,
+	"magick": 8,
+	"miff": 8,
+	"heif": 9,
+	"avif": 10,
+}
+
+var imageTypeToExtension = map[ImageType]string {
+	1: "jpeg",
+	2: "webp",
+	3: "png",
+	4: "tiff",
+	5: "gif",
+	6: "pdf",
+	7: "svg",
+	8: "miff",
+	9: "heif",
+	10: "avif",
+}
+
+var imageTypeToMime = map[ImageType]string {
+	1: "image/jpeg",
+	2: "image/webp",
+	3: "image/png",
+	4: "image/tiff",
+	5: "image/gif",
+	6: "application/pdf",
+	7: "image/svg+xml",
+	8: "image/x-miff",
+	9: "image/heif",
+	10: "image/avif",
+}
+
 func herr(err error) {
     if err != nil {
         log.Println(err)
@@ -26,4 +69,34 @@ func Rotate(bytes []byte, angle int64) []byte {
 	herr(err)
 
 	return img
+}
+
+// Convert takes the image bytes and target format as extension or MIME type
+func Convert(bytes []byte, imgType string) ([]byte, string, string) {
+	it := NewImageType(imgType)
+	img, err := bimg.NewImage(bytes).Convert(bimg.ImageType(it))
+	herr(err)
+
+	ex := it.ToExtension()
+	mime := it.ToMime()
+
+	return img, ex, mime
+}
+
+func (imgType ImageType) ToExtension() string {
+	ex := imageTypeToExtension[imgType]
+
+	return ex
+}
+
+func (imgType ImageType) ToMime() string {
+	mime := imageTypeToMime[imgType]
+
+	return mime
+}
+
+func NewImageType(itype string) ImageType {
+	it := stringToImageType[itype]
+
+	return it
 }
--- image_test.go
+++ image_test.go
@@ -49,3 +49,39 @@ func TestRotate(t *testing.T) {
 	r270 := image.Rotate(img, 270)
 	write(r270, "rotate-270", t)
 }
+
+func TestConvertToJpg(t *testing.T) {
+	img := getImg(t)
+	jpeg, ex, mime := image.Convert(img, "jpeg")
+	if (ex != "jpeg") {
+		t.Errorf("expected: %s, got: %s", "jpeg", ex)
+	}
+	if (mime != "image/jpeg") {
+		t.Errorf("expected: %s, got: %s", "image/jpeg", mime)
+	}
+	write(jpeg, "convert-jpeg", t)
+}
+
+func TestConvertToPng(t *testing.T) {
+	img := getImg(t)
+	png, ex, mime := image.Convert(img, "png")
+	if (ex != "png") {
+		t.Errorf("expected: %s, got: %s", "png", ex)
+	}
+	if (mime != "image/png") {
+		t.Errorf("expected: %s, got: %s", "image/png", mime)
+	}
+	write(png, "convert-png", t)
+}
+
+func TestConvertToGif(t *testing.T) {
+	img := getImg(t)
+	gif, ex, mime := image.Convert(img, "gif")
+	if (ex != "gif") {
+		t.Errorf("expected: %s, got: %s", "gif", ex)
+	}
+	if (mime != "image/gif") {
+		t.Errorf("expected: %s, got: %s", "image/gif", mime)
+	}
+	write(gif, "convert-gif.gif", t)
+}
